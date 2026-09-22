1b66858cf8b9b06e059d8c52edf59d7d7ed9e7b5
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2026-09-22 16:57:42 +0000

    ⭐ s01t0401 for Finaliza toma de tiempo de la suma de 100 numeros usando un for

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -8,14 +8,22 @@ calculara la suma del 1 al 100
 # Importamos biblioteca time
 import time
 
-# Creando una marca de tiempo
+# Tomando el tiempo inicial
 timestamp_01 = time.time()
 
 # Programa que calcula las suma
 # de los "n" numeros naturales
 n = 100
-sum = 0
+total_sum = 0
 
 # Ciclo for
 for number in range(1,n+1):
-    print(str(number) + " ")
+    total_sum = total_sum + number
+
+print(f"La suma de 1 hasta {n} es: {total_sum}")
+
+# Tomando el tiempo final
+timestamp_02 = time.time()
+
+# Impresión del tiempo de ejecución
+print(f"Tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6:.2f} μs")
